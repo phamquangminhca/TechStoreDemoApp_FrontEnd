282d3cad099b418de81d07fc3895df52035cdc31
Quang Minh Pham <phamquangminhca@Quangs-MacBook-Pro.local>
2023-01-03 14:49:11 -0600

    Editted DiscountItem components

--- src/components/DiscountItem.jsx
+++ src/components/DiscountItem.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import discountItemFrame from '../assets/images/discountItemFrame.webp'
+import fireIcon from '../assets/images/fireIcon.png'
+import fiveStar from '../assets/images/fiveStar.png'
+import noStar from '../assets/images/noStar.png'
 
-function DiscountItem({img}) {
+function DiscountItem({img, productName, price, numItemsSold, goodRating}) {
   return (
     <div className='discount-item'>
       <div className='discount-item__white-block'></div>
       <img src={discountItemFrame} alt='' className='discount-item__img-frame'/>
       <img src={img} alt='' className='discount-item__img'/>
+      <div style={{"textAlign":"left"}}>
+        <p style={{"fontSize":"14px", "height":"30px"}}>{productName}</p>
+        {goodRating && <img src={fiveStar} alt='' style={{"width":"80px"}}/>}
+        {!goodRating && <img src={noStar} alt='' style={{"width":"80px"}}/>}  
+        <p style={{"color":"red", "fontWeight":"bold"}}>{price}</p>
+      </div>
+      <div className='dicount-item__numbers-sold'>
+        <img src={fireIcon} alt='' style={{"height":"14px"}}/>
+        <small>{numItemsSold} items have been sold</small>
+      </div>
     </div>
   )
 }
--- src/components/ListDiscountItems.jsx
+++ src/components/ListDiscountItems.jsx
@@ -1,19 +1,68 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import DiscountItem from './DiscountItem'
 import discountItem1 from '../assets/images/discountItem1.webp'
 import discountItem2 from '../assets/images/discountItem2.webp'
 import discountItem3 from '../assets/images/discountItem3.webp'
 import discountItem4 from '../assets/images/discountItem4.webp'
 import discountItem5 from '../assets/images/discountItem5.webp'
+import hotGif from "../assets/images/hotGif.webp"
+import Clock from './Clock'
 
 function ListDiscountItems() {
+
+  const [timerDays, setTimerDays] = useState()
+  const [timerHours, setTimerHours] = useState()
+  const [timerMinutes, setTimerMinutes] = useState()
+  const [timerSeconds, setTimerSeconds] = useState()
+
+  let interval
+
+  const startTimer = () => {
+    const countDownDate = new Date("January 20, 2023").getTime()
+
+    interval = setInterval(() => {
+        const now = new Date().getTime()
+
+        const distance = countDownDate - now
+
+        const days = Math.floor(distance / (24 * 60 * 60 * 1000))
+        const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60))
+        const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60))
+        const seconds = Math.floor((distance % (60 * 1000)) / 1000)
+
+        if (distance < 0) {
+            // Stop Timer
+            clearInterval(interval.current)
+        } else {
+            // Update Timer
+            setTimerDays(days)
+            setTimerHours(hours)
+            setTimerMinutes(minutes)
+            setTimerSeconds(seconds)
+        }
+    })
+  }
+
+  useEffect(() => {
+    startTimer()
+  })
+
   return (
     <div className='list-discount-items'>
-      <DiscountItem img={discountItem1}/>
-      <DiscountItem img={discountItem2}/>
-      <DiscountItem img={discountItem3}/>
-      <DiscountItem img={discountItem4}/>
-      <DiscountItem img={discountItem5}/>
+        <div className='list-discount-items__top'>
+            <span className='list-discount-items__top_text'>SHOCKING 50% SALE</span>
+            <img src={hotGif} alt='' className='list-discount-items__top_img'/>
+            <span style={{"marginLeft":"690px"}}>Ends in</span>
+            <Clock timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSeconds={timerSeconds}/>
+        </div>
+        <div className='list-discount-items__bottom'>
+            <DiscountItem img={discountItem1} productName='Apple Watch Series 7 Aluminium Cellular' price='20.490.000VND' numItemsSold='231' goodRating={true}/>
+            <DiscountItem img={discountItem2} productName='Mozard S21 Bluetooth Speaker' price='550.000VND' numItemsSold='264' goodRating={true}/>
+            <DiscountItem img={discountItem3} productName='eValu Spanker' price='220.000VND' numItemsSold='123' goodRating={false}/>
+            <DiscountItem img={discountItem4} productName='Xmobile Gram 4' price='450.000VND' numItemsSold='183' goodRating={false}/>
+            <DiscountItem img={discountItem5} productName='Xmobile Gram 4 Dull Dog' price='420.000VND' numItemsSold='111' goodRating={false}/>
+        </div>
+
     </div>
   )
 }
